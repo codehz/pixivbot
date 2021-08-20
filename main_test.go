d38b173dbe8619fb001de66b6eb9e3badf261c70
codehz <codehz@outlook.com>
2021-08-20 23:20:50 +0800

    add support for member_illust link

--- main_test.go
+++ main_test.go
@@ -14,6 +14,16 @@ func assertNoError(t *testing.T, err error) {
 	}
 }
 
+func expectError(t *testing.T, err error, message string) {
+	if err != nil {
+		if err.Error() == message {
+			return
+		}
+		t.Fatalf("expected error with message `%s`, but got `%s`", message, err.Error())
+	}
+	t.Fatalf("expected error but not")
+}
+
 func TestParseIllust(t *testing.T) {
 	value, err := parseIllustId("92065303")
 	assertNoError(t, err)
@@ -21,6 +31,15 @@ func TestParseIllust(t *testing.T) {
 	value, err = parseIllustId("https://www.pixiv.net/artworks/92065303")
 	assertNoError(t, err)
 	assertEqual(t, value, 92065303)
+	value, err = parseIllustId("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=92065303")
+	assertNoError(t, err)
+	assertEqual(t, value, 92065303)
+	_, err = parseIllustId("http://www.pixiv.net")
+	expectError(t, err, "not a pixiv link")
+	_, err = parseIllustId("https://www.pixiv.net/invalid")
+	expectError(t, err, "not a illust link")
+	_, err = parseIllustUrl(" ")
+	expectError(t, err, "not a pixiv link")
 }
 
 func TestIsAscii(t *testing.T) {
